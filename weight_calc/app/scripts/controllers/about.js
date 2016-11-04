05befc5ae1a71736370bfcd3c3853a387bc356fa
Thomas Watson <twatson071@gmail.com>
2016-11-04 15:29:32 -0500

    Yeoman fix

---
+++ weight_calc/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weightCalcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the weightCalcApp
+ */
+angular.module('weightCalcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
